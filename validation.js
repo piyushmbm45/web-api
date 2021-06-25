bc8d6cd05a6655524b09f166cf8fa163ceb9f384
Piyush Jain <piyushmbm45@gmail.com>
2021-06-25 16:56:42 +0530

    checking that user put some input or not

--- validation.js
+++ validation.js
@@ -1,31 +1,38 @@
 const inpObj = document.getElementById('id1');
 const button = document.getElementById('btn');
 button.addEventListener('click', function () {
-    // validity property
-    if (!inpObj.checkValidity()) {
-        document.getElementById('demo').innerHTML = inpObj.validationMessage;
-    }
-    else {
-        document.getElementById('demo').innerHTML = 'Input Ok';
-    }
-    // rangeOverflow property
-    let text;
-    if(inpObj.validity.rangeOverflow){
-        text = "Value is Too Large"
+    // firstly checking that user put some input or not 
+    if (!inpObj.validity.valueMissing) {
+        // validity property
+        if (!inpObj.checkValidity()) {
+            document.getElementById('demo').innerHTML = inpObj.validationMessage;
+        }
+        else {
+            document.getElementById('demo').innerHTML = 'Input Ok';
+        }
+        // rangeOverflow property
+        let text;
+        if (inpObj.validity.rangeOverflow) {
+            text = "Value is Too Large"
+        }
+        else {
+            text = "Value is Ok"
+        }
+        document.getElementById("demo1").innerHTML = text;
+        // rangeUnderrflow property
+        let text1;
+        if (inpObj.validity.rangeUnderflow) {
+            text1 = "Value is Too Small"
+        }
+        else {
+            text1 = "Value is Ok"
+        }
+        document.getElementById("demo2").innerHTML = text1;
     }
     else{
-        text = "Value is Ok"
+        document.getElementById('demox').innerHTML = 'Please Input some Number'
     }
-    document.getElementById("demo1").innerHTML = text;
-    // rangeUnderrflow property
-    let text1;
-    if(inpObj.validity.rangeUnderflow){
-        text1 = "Value is Too Small"
-    }
-    else{
-        text1 = "Value is Ok"
-    }
-    document.getElementById("demo2").innerHTML = text1;
+
 
 });
 
