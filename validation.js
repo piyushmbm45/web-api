c75a3907c21eeee1616dbd3a35798bc3e0656fe7
Piyush Jain <piyushmbm45@gmail.com>
2021-06-25 16:45:44 +0530

    overflow property

--- validation.js
+++ validation.js
@@ -1,12 +1,22 @@
 const inpObj = document.getElementById('id1');
 const button = document.getElementById('btn');
 button.addEventListener('click', function () {
+    // validity property
     if (!inpObj.checkValidity()) {
         document.getElementById('demo').innerHTML = inpObj.validationMessage;
     }
     else {
         document.getElementById('demo').innerHTML = 'Input Ok';
     }
+    // rangeOverflow property
+    let text;
+    if(inpObj.validity.rangeOverflow){
+        text = "Value is Too Large"
+    }
+    else{
+        text = "Value is Ok"
+    }
+    document.getElementById("demo1").innerHTML = text;
 });
 
 
